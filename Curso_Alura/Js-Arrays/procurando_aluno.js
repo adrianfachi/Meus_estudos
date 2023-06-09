5181347a8a7539bdbc809e94eb69bb5e0cdaaf42
adrianfachi <133840517+adrianfachi@users.noreply.github.com>
2023-06-09 10:50:02 -0300

    Curso alura

--- Curso_Alura/Js-Arrays/procurando_aluno.js
+++ Curso_Alura/Js-Arrays/procurando_aluno.js
@@ -4,13 +4,13 @@ const medias = [10, 8, 7.5, 9];
 const listaAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno) {
-  if (listaAlunosEMedias[0].includes(aluno)) {
+  if (alunos.includes(aluno)) {
     console.log(`O aluno está cadastrado`);
-    const indice = listaAlunosEMedias[0].indexOf(aluno);
-    console.log(`${aluno} tem a média ${listaAlunosEMedias[1][indice]}`);
+    const indice = alunos.indexOf(aluno);
+    console.log(`${aluno} tem a média ${medias[indice]}`);
   } else {
     console.log("Aluno não encontrado");
   }
 }
 
-exibeNomeENota("Ana");
+exibeNomeENota("Caio");
