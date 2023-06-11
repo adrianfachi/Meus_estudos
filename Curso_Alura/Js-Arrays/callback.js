650fe99b3c36b2340af9db7e656b035a8b2cf2a4
adrianfachi <133840517+adrianfachi@users.noreply.github.com>
2023-06-11 10:03:39 -0300

    callback/ map()

---
+++ Curso_Alura/Js-Arrays/callback.js
@@ -0,0 +1,13 @@
+const nomes = ["Adrian", "Mari", "Camis"];
+
+nomes.forEach(function (nome) {
+    console.log(nome)
+});
+
+nomes.forEach(nome => console.log(nome));
+
+function imprimeNome(nome){
+    console.log(nome)
+}
+
+nomes.forEach(imprimeNome)
---
+++ Curso_Alura/Js-Arrays/map_nomes.js
@@ -0,0 +1,4 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva']
+
+const nomesMaiusculo = nomes.map(maiusculo => maiusculo.toUpperCase())
+console.log(nomesMaiusculo)
