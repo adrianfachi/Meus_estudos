2f5cac93521dac39ec334de9ec10e938aa779197
adrianfachi <133840517+adrianfachi@users.noreply.github.com>
2023-06-14 20:06:30 -0300

    Manipulando o DOM em JavaScript

---
+++ Curso_Alura/Js-DOM/ex1/script.js
@@ -0,0 +1,5 @@
+let botao = document.getElementById("calcular");
+let resultado = document.querySelector(".resultado");
+botao.addEventListener("click", () => {
+		resultado.innerHTML = `Fui clicado`
+	});
